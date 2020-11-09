9c72ebcf98b43dbc27dd47228a38b1add37a4e97
Igor Soares <igor.soares@gold360.com.br>
2020-11-09 10:42:31 -0300

    first commit

---
+++ DeviceListKit/DeviceListKit.h
@@ -0,0 +1,18 @@
+//
+//  DeviceListKit.h
+//  DeviceListKit
+//
+//  Created by Igor Soares on 09/11/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DeviceListKit.
+FOUNDATION_EXPORT double DeviceListKitVersionNumber;
+
+//! Project version string for DeviceListKit.
+FOUNDATION_EXPORT const unsigned char DeviceListKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DeviceListKit/PublicHeader.h>
+
+
---
+++ DeviceListKitExamples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DeviceListKitExamples
+//
+//  Created by Igor Soares on 09/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
